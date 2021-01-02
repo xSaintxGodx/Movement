bb80320697c093d47782a8c9c1d488263ac10664
Wesley <wesleyhokage123@outlook.com>
2021-01-02 00:46:46 -0300

    Movement Basic
    Player Controller and Movement scripts added.
    made the Basic Movement.
    Flip done too.

---
+++ Movement_I/Assets/2.Scripts/Player/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    bool forward = true;
+    
+    public void Move(Rigidbody2D _rb)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        _rb.velocity = new Vector2(horizontal * Time.fixedDeltaTime * speed, _rb.velocity.y);
+        if (horizontal > 0f)
+        {
+            if(!forward)
+                Flip();
+        }
+        else if (horizontal < 0f)
+        {
+            if(forward)
+                Flip();
+        }
+    }
+    
+    void Flip()
+    {
+        forward = !forward;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
+
---
+++ Movement_I/Assets/2.Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Movement movement;
+
+    private Rigidbody2D _rb;
+    void Awake()
+    {
+        movement = FindObjectOfType<Movement>();
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.Move(_rb);
+    }
+}
