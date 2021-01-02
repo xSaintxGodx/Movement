95058d1e405614bf4f47bd150084d91f58a6c6fd
Wesley <wesleyhokage123@outlook.com>
2021-01-02 01:19:54 -0300

    Animation and alteration
    Configured animation for Idle and Run.
    Small refactoring for better understanding.

--- Movement_I/Assets/2.Scripts/Player/Movement.cs
+++ Movement_I/Assets/2.Scripts/Player/Movement.cs
@@ -1,30 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    float horizontalInput;
     public float speed;
 
     bool forward = true;
     
-    public void Move(Rigidbody2D _rb)
+    public void Move(Rigidbody2D _rb, Animator _anim)
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector2(horizontalInput * Time.fixedDeltaTime * speed, _rb.velocity.y);
 
-        _rb.velocity = new Vector2(horizontal * Time.fixedDeltaTime * speed, _rb.velocity.y);
-        if (horizontal > 0f)
+        _anim.SetInteger("Movement", (int)_rb.velocity.x);
+        _anim.SetBool("isIdle", false);
+
+        if (horizontalInput == 0 & Input.GetButtonDown("Horizontal") == false)
+            _anim.SetBool("isIdle", true);
+
+        checkFlip();
+    }
+
+    void checkFlip()
+    {
+        if (horizontalInput > 0f)
         {
             if(!forward)
                 Flip();
         }
-        else if (horizontal < 0f)
+        else if (horizontalInput < 0f)
         {
             if(forward)
                 Flip();
         }
     }
-    
     void Flip()
     {
         forward = !forward;
--- Movement_I/Assets/2.Scripts/Player/PlayerController.cs
+++ Movement_I/Assets/2.Scripts/Player/PlayerController.cs
@@ -8,15 +8,17 @@ public class PlayerController : MonoBehaviour
     Movement movement;
 
     private Rigidbody2D _rb;
+    Animator _anim;
     void Awake()
     {
         movement = FindObjectOfType<Movement>();
 
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement.Move(_rb);
+        movement.Move(_rb, _anim);
     }
 }
